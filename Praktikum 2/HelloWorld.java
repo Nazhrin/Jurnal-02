79f921899aafbd6e21da16edb7d465dc56b24064
unknown <nnazhrin@gmail.com>
2020-09-16 19:20:25 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama:Nazhrin Nazarudin Achmad");
+		System.out.println("NIM:6706200098");
+		System.out.println("Kelas:D3IF4402");
+	}
+}
